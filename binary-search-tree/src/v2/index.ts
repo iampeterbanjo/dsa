fafe9cb0688e72883bb08f5ead40819d8e10ce31
Peter Banjo <me@iampeterbanjo.com>
2022-06-28 22:04:02 +0000

    fix deleting node

--- binary-search-tree/src/v2/index.ts
+++ binary-search-tree/src/v2/index.ts
@@ -45,11 +45,51 @@ export class BST {
     return this;
   }
 
-  // set child node to null
-  nullify(child: BST) {
+  // update child node
+  updateChild(child: BST, update: BST | null) {
     const side = this.getSide(child.value);
-    this[side] = null;
+    this[side] = update;
 
+    return this;
+  }
+
+  // handle delete root as special case
+  deleteRoot() {
+    if (this.right && this.left) {
+      this.replaceWithSuccessor();
+      return this;
+    }
+
+    if (this.left) {
+      this.value = this.left.value;
+      this.right = this.left.right;
+      this.left = this.left.left;
+      return this;
+    }
+
+    if (this.right) {
+      this.value = this.right.value;
+      this.left = this.right.left;
+      this.right = this.right.right;
+      return this;
+    }
+
+    // single-node so do nothing
+    return this;
+  }
+
+  // null or minimum value in right subtree
+  replaceWithSuccessor() {
+    const { node: successor, parent } = this.right!.getBottomLeft();
+    this.value = successor.value;
+
+    // no left subtree
+    if (!parent) {
+      this.right = null;
+      return this;
+    }
+
+    parent.left = null;
     return this;
   }
 
@@ -57,30 +97,37 @@ export class BST {
   remove(value: number): BST {
     const { node: remove, parent } = this.find(value);
 
-    // must be root
+    if (!remove) {
+      console.error("Node not found: ", value);
+      return this;
+    }
+
+    // no parent means we are removing the root node
     if (!parent) {
-      throw "Cannot remove root";
-    }
-
-    // if removed node's right subtree is empty
-    // replace with left subtree
-    if (remove.right === null && remove.left) {
-      remove.right = remove.left;
+      remove.deleteRoot();
       return this;
     }
 
-    // no children
-    if (remove.right === null && remove.left === null) {
-      parent.nullify(remove);
+    // both children
+    if (remove.right && remove.left) {
+      remove.replaceWithSuccessor();
       return this;
     }
 
-    // else replace with leftmost node in right subtree
-    const { node: min, parent: minParent } = remove.right!.getBottomLeft();
-    remove.setValue(min.value);
+    // skip removed node with a left subtree first
+    if (parent.left === remove) {
+      parent.left = remove.left ? remove.left : remove.right;
 
-    minParent?.nullify(min);
+      return this;
+    }
 
+    if (parent.right === remove) {
+      parent.right = remove.left ? remove.left : remove.right;
+
+      return this;
+    }
+
+    // must have no children
     return this;
   }
 
@@ -88,20 +135,18 @@ export class BST {
   find(
     value: number,
     parent: BST | null = null
-  ): { node: BST; parent: BST | null } {
+  ): { node: BST | null; parent: BST | null } {
     if (this.value === value) {
       return { node: this, parent };
     }
 
     const next = this.getSide(value);
+    // it should be here but we cant find it
     if (this[next] === null) {
-      throw new Error(`Missing node with value: ${value}`);
+      return { node: null, parent: null };
     }
 
-    if (this[next]?.value === value) {
-      return { node: this, parent };
-    }
-    return this.find(value, this);
+    return this[next]!.find(value, this);
   }
 
   // find node successor
