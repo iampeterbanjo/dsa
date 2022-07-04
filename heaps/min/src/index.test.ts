ba2393144b3339eb0d83d682ab7e7b480ad52f23
Peter Banjo <me@iampeterbanjo.com>
2022-07-04 21:28:26 +0000

    add remove method. refactor methods for clarity

--- heaps/min/src/index.test.ts
+++ heaps/min/src/index.test.ts
@@ -1,26 +1,47 @@
 import assert from 'assert';
 import { MinHeap } from './index';
-/**
- * Given an array of random integers
- * Then build a min heap
- */
-const checkBuiltHeapIsCorrect = (array: number[], correct: number[]) => {
-  const { heap } = new MinHeap(array);
 
+const expectArrayContaining = (array: any[], correct: any[]) => {
   try {
     for (let i = 0; i < correct.length; i++) {
-      const result = heap[i + 1];
+      const result = array[i + 1];
       const expected = correct[i];
       assert(result === expected, `received ${result} but expected ${expected} at ${i}`);
     }
   } catch (error) {
     console.error(error);
-    console.log(heap);
+    console.log(array);
   }
 }
 
+/**
+ * Given an array of random integers
+ * Then build a min heap
+ */
+const testBuiltHeapIsCorrect = (array: number[], correct: number[]) => {
+  const { heap } = new MinHeap(array);
 
-checkBuiltHeapIsCorrect([1,2,3], [1,2,3]);
-checkBuiltHeapIsCorrect([1, 2, 0], [0, 2, 1]);
-checkBuiltHeapIsCorrect([7, 8, 0, 2, 6, 8, 24], [0, 2, 7, 8, 6, 8, 24]);
+  expectArrayContaining(heap, correct);
+}
+
+
+testBuiltHeapIsCorrect([1,2,3], [1,2,3]);
+testBuiltHeapIsCorrect([1, 2, 0], [0, 2, 1]);
+testBuiltHeapIsCorrect([7, 8, 0, 2, 6, 8, 24], [0, 2, 7, 8, 6, 8, 24]);
+
+/**
+ * Given an array of random integers
+ * And a correctly built heap
+ * When the root node is deleted
+ * Then the resulting heap is correct
+ */
+const testDeleteRootIsCorrect = (array: number[], correct: number[]) => {
+  const minHeap = new MinHeap(array);
+
+  minHeap.remove();
+
+  expectArrayContaining(minHeap.heap, correct);
+}
+
+testDeleteRootIsCorrect([7, 8, 0, 2, 6, 8, 24], [2, 6, 7, 8, 24, 8])
 
--- heaps/min/src/index.ts
+++ heaps/min/src/index.ts
@@ -9,9 +9,29 @@ export class MinHeap {
     this.buildHeap(array);
   }
 
+  // get first index position
+  get firstIndex() {
+    return 1;
+  }
+
+  // last index position
+  get lastIndex() {
+    return this.heap.length - 1;
+  }
+
+  // get item at index
+  item(index: number) {
+    return this.heap[index];
+  }
+
   // get parent
   parentIndex(index: number) { // 3
     return Math.floor(index / 2); // 1
+  }
+
+  // get item at
+  parent(index: number) {
+    return this.heap[this.parentIndex(index)]
   }
 
   // get the left child
@@ -21,7 +41,32 @@ export class MinHeap {
 
   // get right child
   rightChildIndex(index: number) {
-    return (2 * index) + 1;
+    return this.leftChildIndex(index) + 1;
+  }
+
+  // get left child
+  leftChild(index: number)  {
+    return this.heap[this.leftChildIndex(index)];
+  }
+
+  // get right child
+  rightChild(index: number) {
+    return this.heap[this.rightChildIndex(index)];
+  }
+
+  // check parent exists
+  hasParent(index: number) {
+    return this.parentIndex(index) > 0;
+  }
+
+  // check left child exists
+  hasLeftChild(index: number) {
+    return this.leftChildIndex(index) < this.lastIndex;
+  }
+
+  // check right child exists
+  hasRightChild(index: number) {
+    return this.rightChildIndex(index) < this.lastIndex;
   }
 
   // given an array of random integers
@@ -52,9 +97,24 @@ export class MinHeap {
     [this.heap[target], this.heap[src]] = [this.heap[src], this.heap[target]];
   }
 
-
+  // make sure root node's value is in the right position
   siftDown() {
-    // Write your code here.
+    let index = 1;
+
+    // [ -Infinity, 24, 2, 7, 8, 6, 8 ]
+    // [ -Infinity, 2, 6, 7, 8, 24, 8 ]
+    // if there is no left child, there definitely is no right child
+    while (this.hasLeftChild(index)) { // 2 -> 4
+      const smallerChildIndex = this.leftChild(index) > this.rightChild(index) ? this.rightChildIndex(index) : this.leftChildIndex(index); // 5
+
+      if (this.heap[smallerChildIndex] > this.heap[index]) { // 6 > 24
+        // nothing to do
+        break;
+      }
+
+      this.swap(index, smallerChildIndex); // 2, 5
+      index = smallerChildIndex; // 5
+    }
   }
 
   // assuming we're sifting-up the last inserted item
@@ -62,10 +122,10 @@ export class MinHeap {
   siftUp() {
     // [-, 1, 2, 0, ...]
     // [0, 1, 2, 3]
-    let index = this.heap.length - 1; // 3
+    let index = this.lastIndex; // 3
 
     // heap[3] < heap[1] : 0 < 1: true
-    while (this.heap[index] < this.heap[this.parentIndex(index)]) {
+    while (this.hasParent(index) && this.heap[index] < this.parent(index)) {
       this.swap(index, this.parentIndex(index)); // heap, 3, 1
       index = this.parentIndex(index); // 1
     }
@@ -76,9 +136,12 @@ export class MinHeap {
     return -1;
   }
 
+  // remove the root node
   remove() {
-    // Write your code here.
-    return -1;
+    // place successor at root
+    this.heap[this.firstIndex] = this.heap[this.lastIndex] // [ -Infinity, 24, 2, 7, 8, 6, 8, 24 ]
+    this.heap = this.heap.splice(0, this.lastIndex); // [ -Infinity, 24, 2, 7, 8, 6, 8 ]
+    this.siftDown();
   }
 
   insert(value: number) {
